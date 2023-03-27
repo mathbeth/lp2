785c009511b5d22ee8b4101247d55303c90f9cad
Matheus <matheus-nascimento.mn@academico.ifpb.edu.br>
2023-03-27 09:00:23 -0300

    hello-api

---
+++ express/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const server = express(); 
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+server.get('/ola', (req, res) => {
+  res.send('Olá, Mundo');
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
